34f3aeda0c41c66f5a02044092ed9c9c486f2505
rui <afor.ruigmail.com>
2020-01-31 12:27:48 +0100

    rui

--- shoplist/src/components/functions.js
+++ shoplist/src/components/functions.js
@@ -1,18 +1,44 @@
-export function toglePw(id) {
+
+/* export function toglePw(id) {
     var x = document.getElementById(id)
 
     if(x.type === "password")
         x.type = "text";
     else
         x.type = "password";
-}
+} */
 
 
 
-export function showHide(){
+/* export function showHide(){
     this.setState({
       type: this.state.type === 'password' ? 'input' : 'password'
     })  
   }
 
-  export default showHide; toglePw;
+  import fetch from 'isomorphic-fetch';
+ */
+  // * snip *
+  
+export function createUser(url,data) {
+      return fetch(url, {
+          method: 'POST',
+          //mode: 'CORS',
+          body: JSON.stringify(data),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      }).then(res =>res.json()) 
+          
+      .catch(err => err)
+
+      .then(res=>{
+          console.log(res);
+      })
+
+  } 
+
+
+/*   export function test(){
+      console.log("i'm a function");
+  } */
