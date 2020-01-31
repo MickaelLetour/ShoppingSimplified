6176033b597e8fe7196609c928495f4694f42be1
Letpour Mickael <mickael.letour@hotmail.fr>
2020-01-31 09:52:08 +0100

    start react

---
+++ shoplist/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header.js"
+import Login from "./components/Login.js"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Login />
+        </div>
+    )
+}
+
+export default App;
