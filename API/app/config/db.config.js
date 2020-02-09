9f7489bffb9563ad8d19283d1ddad91bdcb42b9b
Letpour Mickael <mickael.letour@hotmail.fr>
2020-02-09 20:27:21 +0100

    attention

--- API/app/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "RGML",
-    PASSWORD: "12121312",
-    DB: "ShoppingSimplified"
-  };
--- shoplist/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import Routes from "./Routes.js";
-
-
-
-ReactDOM.render(<Routes />, document.getElementById("mainContainer"))
